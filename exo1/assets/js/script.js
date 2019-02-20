639562ba4eee05ae1211c11c2652d8223216660e
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-20 14:51:32 +0100

    exo corrigé

--- exo1/assets/js/script.js
+++ exo1/assets/js/script.js
@@ -1,6 +1,6 @@
-var testApp = angular.module('app',[]);
+var testApp = angular.module('app',[]); //création du module
 
-testApp.controller('testCtrl', function($scope){
+testApp.controller('testCtrl', function($scope){ // creation du controller
   $scope.firstSentence = 'L\'Axolotl, Ambystoma mexicanum, est une espèce d\'urodèles de la famille des Ambystomatidae.';
   $scope.secondSentence = 'L\'ORNITHORYNQUE (ORNITHORHYNCHUS ANATINUS) EST UN ANIMAL SEMI-AQUATIQUE ENDÉMIQUE DE L\'EST DE L\'AUSTRALIE, Y COMPRIS LA TASMANIE.';
 });
